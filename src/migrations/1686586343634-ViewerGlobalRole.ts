1fc03fe7faf9c00534befe467b8b137ac416f41f
Sam Hurwitz <contact.samhurwitz@gmail.com>
2023-06-13 00:38:55 +0500

    fix: migration viewer role

--- src/migrations/1686586343634-ViewerGlobalRole.ts
+++ src/migrations/1686586343634-ViewerGlobalRole.ts
@@ -1,12 +1,12 @@
 import { MigrationInterface, QueryRunner } from 'typeorm'
 
-export class ViewerGlobalRole1686585343634 implements MigrationInterface {
+export class ViewerGlobalRole1686586343634 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`INSERT INTO roles_permissions (role_id, permission_id)
     SELECT r.id, p.id
     FROM role r
     JOIN permission p ON r.name = 'Viewer'
-    WHERE p.name = 'projects:read';
+    WHERE p.name = 'project:read';
     `)
   }
 
